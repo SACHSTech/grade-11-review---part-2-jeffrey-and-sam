d08d3d3b5078313d0e0da5a569992fb55f0202db
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-11-25 15:37:00 +0000

    Finished File Io 2.

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,22 +1,26 @@
 package gr11review.part2;
 
 import java.io.*;
+import java.util.*;
 //javac -d bin src/gr11review/part2/Utility.java
 //java -classpath bin gr11review.part2.Utility
 public class Utility {
 
-  public static int sumNumbers(String str){
-    String strSum = "";
-    int intTotal = 0;
+  public static int sumNumbers(String str) {
+    String strSum;
+    int intTotal;
     int intLength = str.length();
     char chrCharacter;
+    strSum = "";
+    intTotal = 0;
 
-    for(int intCount = 0; intCount < intLength; intCount++){
+    for (int intCount = 0; intCount < intLength; intCount++) {
       chrCharacter = str.charAt(intCount);
 
-      if(Character.isDigit(chrCharacter)){
+      if (Character.isDigit(chrCharacter)) {
         strSum += chrCharacter;
-      }else if(!strSum.equals("")){
+      } 
+      else if (!strSum.equals("")) {
         intTotal += Integer.parseInt(strSum);
         strSum = "";
       }
@@ -25,9 +29,27 @@ public class Utility {
 
   }
 
-  public static void main(String[] args){
-    System.out.println(sumNumbers("1a1a1a1"));
+  public static String alphaWord(String filenametxt) throws IOException {
+    BufferedReader theFile = new BufferedReader(new FileReader("src/gr11review/part2/" + filenametxt + ".txt"));
+    String strLine = "";
+    String strAlpha;
+    strAlpha = theFile.readLine();
+    while (strLine != null) {
+      strLine = theFile.readLine();
+      if (strLine != null && strAlpha.compareTo(strLine) > 0) {
+        strAlpha = strLine;
+      }
+    }
+    theFile.close();
+    return strAlpha;
 
+  }
+
+
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(sumNumbers("1a1a1a1"));
+    System.out.println(alphaWord("file"));
   }
 
 
