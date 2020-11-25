d0f242b337b37e1fdcbe6abd54e7c58f58ca29a6
samliu12 <sam.liu21@ycdsbk12.ca>
2020-11-25 15:32:31 +0000

    Finished file io 1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,5 +1,16 @@
 package gr11review.part2;
 
+import Utility;
+
 public class Main {
+
+  /*
+  javac -d bin src/gr11review/part2/Main.java
+  java -classpath bin gr11review.part2.Main
+  */
+
+  public static void main(String[] args) {
+    System.out.println(Utility.zipZap("zopzip"));
+  }
 
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -9,13 +9,14 @@ public class Utility {
   java -classpath bin gr11review.part2.Utility
   */
 
+
   // SAM
-  // Function that removes the middle letter of any 3 letter word beginning with z and ending with p
+  // Function that takes in a string and removes the middle letter of any 3 letter word beginning with z and ending with p
   public static String zipZap(String strInput) {
     // Declare variable
     String strOutput;
 
-    // Initialize variable appropriately
+    // Initialize variable 
     strOutput = "";
 
     // Iterate through the string and check if the letter i is 'z' and the letter i+2 is 'p'. If so, add 'zp' to the output and increment i to skip past the 'p'. If not, add character i to the output
@@ -32,17 +33,38 @@ public class Utility {
     // Return output string
     return strOutput;
   }
+
+
+  // SAM
+  // Function that returns the longest word in a given text file
+  public static String longestWord(String strFileName) throws IOException {
+    // Create buffered reader tied to FileIO.txt file
+    BufferedReader file = new BufferedReader(new FileReader(strFileName));
+
+    // Declare variables
+    String strLongestWord;
+    String strCurrentWord;
+
+    // Initialize variables 
+    strLongestWord = "";
+    strCurrentWord = file.readLine();
+    
+    // While the end of the given file hasn't been reached, read the new string. If the new string is as long or longer than the current longest word, update the longest word
+    while (strCurrentWord != null) {
+      if (strCurrentWord.length() >= strLongestWord.length()) {
+        strLongestWord = strCurrentWord;
+      }
+      strCurrentWord = file.readLine();
+    }
+
+    // Return longest word
+    return strLongestWord;
+  }
    
 
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.print("Word: ");
-    String str = br.readLine();
-    while (str != "") {
-      System.out.println(zipZap(str));
-      System.out.print("Word: ");
-      str = br.readLine();
-    }
+    System.out.println(longestWord("src/gr11review/part2/FileIO.txt"));
   }
 
 }
