0710ef6afaa2585ae0932de48331a2114473d5f1
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-11-25 15:17:43 +0000

    Finished method 2.

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,36 @@
 package gr11review.part2;
 
+import java.io.*;
+//javac -d bin src/gr11review/part2/Utility.java
+//java -classpath bin gr11review.part2.Utility
 public class Utility {
-    
+
+  public static int sumNumbers(String str){
+    String strSum = "";
+    int intTotal = 0;
+    int intLength = str.length();
+    char chrCharacter;
+
+    for(int intCount = 0; intCount < intLength; intCount++){
+      chrCharacter = str.charAt(intCount);
+
+      if(Character.isDigit(chrCharacter)){
+        strSum += chrCharacter;
+      }else if(!strSum.equals("")){
+        intTotal += Integer.parseInt(strSum);
+        strSum = "";
+      }
+    }
+    return intTotal;
+
+  }
+
+  public static void main(String[] args){
+    System.out.println(sumNumbers("1a1a1a1"));
+
+  }
+
+
+
+
 }
