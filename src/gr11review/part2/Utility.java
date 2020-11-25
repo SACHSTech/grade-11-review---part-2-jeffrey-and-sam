342b7bbe5086c9d9f02ff21526bc4055d0d7cbc5
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-11-25 16:05:51 +0000

    Finished array 2

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -45,11 +45,24 @@ public class Utility {
 
   }
 
-
+  public static int[] notAlone(int[] nums, int value) {
+    int intLength = nums.length;
+    for (int intCount = 1; intCount < intLength-1; intCount++) {
+      if (nums[intCount] == value) {
+        if (nums[intCount-1] != nums[intCount] && nums[intCount+1] != nums[intCount]) {
+          nums[intCount] = Math.max(nums[intCount-1], nums[intCount+1]);
+        }
+      }
+    }
+    return nums;
+  }
 
   public static void main(String[] args) throws IOException {
     System.out.println(sumNumbers("1a1a1a1"));
     System.out.println(alphaWord("file"));
+    int[] num = {3, 2, 3};
+    num = notAlone(num, 2);
+    System.out.println(num[0] + " " + num[1] + " " + num[2]);
   }
 
 
