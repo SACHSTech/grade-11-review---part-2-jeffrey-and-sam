8665c6a07922f09e2de9d4e9e8adf84d0d657cf7
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-11-26 13:55:51 +0000

    Fixed some formatting.

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,20 @@
 package gr11review.part2;
+import java.io.*;
 
+public class Main{
+
+public static void main(String[] args) throws IOException {
+    
+    System.out.println(Utility.sumNumbers("1a1a1a1"));
+    System.out.println(Utility.alphaWord("file"));
+    int[] num = {3, 2, 3};
+    num = Utility.notAlone(num, 2);
+    System.out.println(num[0] + " " + num[1] + " " + num[2]);
+    int[] num2 = {2, 1, 1, 2, 1};
+    System.out.println(Utility.canBalance(num2));
+    Utility.diagonal(7);
+
+  }
+
+
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -2,21 +2,21 @@ package gr11review.part2;
 
 import java.io.*;
 import java.util.*;
-//javac -d bin src/gr11review/part2/Utility.java
-//java -classpath bin gr11review.part2.Utility
+
 public class Utility {
 
   public static int sumNumbers(String str) {
+
     String strSum;
-    int intTotal;
-    int intLength = str.length();
+    int intTotal, intLength;
     char chrCharacter;
     strSum = "";
     intTotal = 0;
+    intLength = str.length();
 
     for (int intCount = 0; intCount < intLength; intCount++) {
+  
       chrCharacter = str.charAt(intCount);
-
       if (Character.isDigit(chrCharacter)) {
         strSum += chrCharacter;
       } 
@@ -24,53 +24,73 @@ public class Utility {
         intTotal += Integer.parseInt(strSum);
         strSum = "";
       }
+
     }
+
     return intTotal;
 
   }
 
   public static String alphaWord(String filenametxt) throws IOException {
+
     BufferedReader theFile = new BufferedReader(new FileReader("src/gr11review/part2/" + filenametxt + ".txt"));
-    String strLine = "";
+
+    String strLine;
     String strAlpha;
     strAlpha = theFile.readLine();
+    strLine = "";
+
     while (strLine != null) {
+
       strLine = theFile.readLine();
       if (strLine != null && strAlpha.compareTo(strLine) > 0) {
         strAlpha = strLine;
       }
+
     }
+
     theFile.close();
     return strAlpha;
 
   }
 
   public static int[] notAlone(int[] nums, int value) {
-    int intLength = nums.length;
+
+    int intLength;
+    intLength = nums.length;
+
     for (int intCount = 1; intCount < intLength-1; intCount++) {
+
       if (nums[intCount] == value) {
         if (nums[intCount-1] != nums[intCount] && nums[intCount+1] != nums[intCount]) {
           nums[intCount] = Math.max(nums[intCount-1], nums[intCount+1]);
         }
       }
+
     }
+
     return nums;
+
   }
 
   public static boolean canBalance(int[] nums){
+
     int intLength;
-    intLength = nums.length;
     int intSum;
-    intSum = 0;
     int intPreSum;
+    intLength = nums.length;
+    intSum = 0;
     intPreSum = 0;
 
 
     for (int intCount = 0; intCount < intLength; intCount++) {
+
       intSum += nums[intCount];
+
     }
 
     for (int intCount = 0; intCount < intLength; intCount++) {
+
       intPreSum += nums[intCount];
       if (intSum - intPreSum == intPreSum) {
         return true;
@@ -78,6 +98,7 @@ public class Utility {
       else if (intSum - intPreSum < intPreSum) {
         return false;
       }
+
     }
 
     return false;
@@ -90,7 +111,9 @@ public class Utility {
 
     String strLine;
     strLine = "";
+
     for (int intCount = 1; intCount <= n; intCount++) {
+
       for (int intCount2 = 0; intCount2 < n - intCount; intCount2++) {
         strLine += "0, ";
       }
@@ -98,29 +121,15 @@ public class Utility {
       for (int intCount2 = 0; intCount2 < intCount - 1; intCount2++) {
         strLine += "2, ";
       }
-      strLine = strLine.substring(0, strLine.length()-1);
 
+      strLine = strLine.substring(0, strLine.length()-1);
       theOut.println(strLine);
       strLine = "";     
+
     }
+
     theOut.close();
-  }
-
-
-  public static void main(String[] args) throws IOException {
-    
-    System.out.println(sumNumbers("1a1a1a1"));
-    System.out.println(alphaWord("file"));
-    int[] num = {3, 2, 3};
-    num = notAlone(num, 2);
-    System.out.println(num[0] + " " + num[1] + " " + num[2]);
-    int[] num2 = {2, 1, 1, 2, 1};
-    System.out.println(canBalance(num2));
-    diagonal(7);
 
   }
-
-
-
 
 }
