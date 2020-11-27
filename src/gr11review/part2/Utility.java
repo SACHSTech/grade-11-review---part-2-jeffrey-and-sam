26f6216192759a299c4f47da061ea9216fa4d8a8
samliu12 <sam.liu21@ycdsbk12.ca>
2020-11-27 13:58:40 +0000

    changed styling of code

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -3,13 +3,18 @@ package gr11review.part2;
 import java.io.*;
 /**
 * A library of grade 11 review functions
-* @author S. Liu
+* @author S.Liu
 *
 */
 public class Utility {
 
-  // SAM
-  // Function that takes in a string and removes the middle letter of any 3 letter word beginning with z and ending with p
+  /**
+  * Takes in a string and removes the middle letter of any 3 letter word beginning with z and ending with p
+  *
+  * @param strInput The given string
+  * @return input string without the middle letter between any z's and p's
+  * @author S.Liu
+  */
   public static String zipZap(String strInput) {
 
     // Declare variable
@@ -18,7 +23,7 @@ public class Utility {
     // Initialize variable 
     strOutput = "";
 
-    // Iterate through the string and check if the letter i is 'z' and the letter i+2 is 'p'. If so, add 'zp' to the output and increment i by 2 to skip to the letter after 'p'. If not, add character i to the output
+    // Iterate through the string and check if the letter i is 'z' and the letter i + 2 is 'p'. If so, add 'zp' to the output and increment i by 2 to skip to the letter after 'p'. If not, add character i to the output
     for (int i = 0; i < strInput.length(); i++) {
       if (i + 2 < strInput.length() && strInput.charAt(i) == 'z' && strInput.charAt(i + 2) == 'p') {
         strOutput += "zp";
@@ -35,8 +40,13 @@ public class Utility {
   }
 
 
-  // SAM
-  // Function that returns the longest word in a given text file
+  /**
+  * Returns the longest word in a given text file
+  *
+  * @param strFileName The name of the file
+  * @return longest word in the file
+  * @author S.Liu
+  */
   public static String longestWord(String strFileName) throws IOException {
 
     // Create buffered reader 
@@ -67,8 +77,13 @@ public class Utility {
   }
 
 
-  // SAM 
-  // Function that takes an integer array. For each multiple of 10, every non-multiple of 10 after that is turned into that multiple of 10 until another multiple of 10 is reached
+  /**
+  * For each multiple of 10 in an integer array, every non-multiple of 10 after that is turned into that multiple of 10 until another multiple of 10 is reached
+  *
+  * @param intNums The integer array
+  * @return the updated array
+  * @author S.Liu
+  */
   public static int[] tenRun(int[] intNums) {
 
     // Declare variables
@@ -93,8 +108,14 @@ public class Utility {
   }
 
 
-  // SAM
-  // Function that takes in two integer arrays sorted in increasing order and checks if all the numbers in the inner array appear in the outer array
+  /**
+  * Takes in two integer arrays sorted in increasing order and checks if all the numbers in the inner array appear in the outer array
+  *
+  * @param intOuter The outer array
+  * @param intInner The inner array
+  * @return if all elements in the innner array are in the outer array
+  * @author S.Liu
+  */
   public static boolean linearIn(int[] intOuter, int[] intInner) {
 
     // Declare variables
@@ -104,7 +125,7 @@ public class Utility {
     // Initialize variables
     intOuterCounter = 0;
 
-    // Iterate through each element of the inner array, looking for a match with the outer array by an incrementing intOuterCounter varaible
+    // Iterate through each element of the inner array, looking for a match with the outer array by an incrementing the intOuterCounter varaible
     for (intInnerCounter = 0; intInnerCounter < intInner.length; intInnerCounter++) {
       while (intOuterCounter < intOuter.length) {
         // If the outer item is greater than the current inner item, that means that there's no match for the current inner item; return false
@@ -131,8 +152,13 @@ public class Utility {
   }
 
 
-  // SAM
-  // Function that prints the pascal triangle up to the given row i and column j
+  /**
+  * Prints pascal's triangle up to the given row i and column j
+  *
+  * @param i The row number
+  * @param j The column number
+  * @author S.Liu
+  */
   public static void pascalTri(int i, int j) throws IOException {
 
     // Initialize print writer
@@ -143,7 +169,7 @@ public class Utility {
     int intColCounter;
     int intPascalTriangle[][] = new int[i][j];
 
-    // Set the first row and column equal to 0
+    // Set the first row and column equal to 1
     for (intRowCounter = 0; intRowCounter < i; intRowCounter++) {
       intPascalTriangle[intRowCounter][0] = 1;
     }
@@ -151,7 +177,7 @@ public class Utility {
       intPascalTriangle[0][intColCounter] = 1;
     }
 
-    // Iterate the row from 1 to i-1. For each of these rows, iterate the column from 1 to j-1. The pascal triangle value of cell[i][j] is the sum of the pascal triangle values to its left and above
+    // Iterate the row from 1 to i-1. For each of these rows, iterate the column from 1 to j-1. The pascal's triangle value of cell[i][j] is the sum of the pascal's triangle values to its left and above
     for (intRowCounter = 1; intRowCounter < i; intRowCounter++) {
       for (intColCounter = 1; intColCounter < j; intColCounter++) {
         intPascalTriangle[intRowCounter][intColCounter] = intPascalTriangle[intRowCounter - 1][intColCounter] + intPascalTriangle[intRowCounter][intColCounter - 1];
